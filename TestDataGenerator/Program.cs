4002002893b91c4d0cac3df15c8c580bcdaa0b6c
Mustafa Ulaş Güney <mustafaulasguney@gmail.com>
2022-07-07 10:47:12 +0300

    fix #4

--- TestDataGenerator/Program.cs
+++ TestDataGenerator/Program.cs
@@ -49,17 +49,21 @@ var configFile = File.ReadAllText(configFilePath);
 var config = JsonSerializer.Deserialize<GeneralConfig>(configFile);
 
 var ipAddressGenerator = new IpAddressGenerator();
-var macAddressGenerator = new MacAddressGenerator(config!.MacConfig);
+var macAddressGenerator = new MacAddressGenerator(config!.MacConfig!);
 var hostnameGenerator = new HostnameGenerator();
 
 if (File.Exists(config.OutputFile))
     File.Delete(config.OutputFile);
 
 ipAddressGenerator.GenerationControl(config.NumberOfGenerations);
+Console.WriteLine($"Program will generate {config.NumberOfGenerations} Ip,Mac and Hostname");
+
 var listOfLines = new List<string>();
-for (int i = 1; i <= config.NumberOfGenerations; i++)
+
+
+for (var i = 1; i <= config.NumberOfGenerations; i++)
 {
-    listOfLines.Add(config.Format
+    listOfLines.Add(config.Format!
             .Replace("$mac$",macAddressGenerator.Generate(config.GenerateUniqueMacs))
             .Replace("$ip$", ipAddressGenerator.Generate(config.GenerateUniqueIps))
             .Replace("$hostname$", hostnameGenerator.Generate(i)));
@@ -68,7 +72,22 @@ for (int i = 1; i <= config.NumberOfGenerations; i++)
         File.AppendAllLines(config.OutputFile!, listOfLines);
         listOfLines.Clear();
     }
+    var progress = i * 100 / config.NumberOfGenerations;
+    switch (progress)
+    {
+        case 25:
+            Console.WriteLine("%25 is completed");
+            break;
+        case 50:
+            Console.WriteLine("%50 is completed");
+            break;
+        case 75:
+            Console.WriteLine("%75 is completed");
+            break;
+    }
 }
+Console.WriteLine($"Path is {config.OutputFile}");
+
 if (listOfLines.Any())
     File.AppendAllLines(config.OutputFile!, listOfLines);
     
